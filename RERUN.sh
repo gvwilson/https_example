78d542428e19d9e90058dd771f4be586878bc314
Greg Wilson <gvwilson@third-bit.com>
2024-02-26 11:09:25 -0500

    fix: change to CN=localhost when creating server cert
    Does not solve problem.

--- RERUN.sh
+++ RERUN.sh
@@ -9,7 +9,7 @@ openssl req -x509 -newkey RSA -nodes -keyout CA.key -days 10 -out CA.pem -reqext
 
 # create server key and cert
 openssl req -new -newkey RSA -nodes -keyout server.key -out server.csr -batch \
-	-reqexts v3_req -subj "/CN=example.com"
+	-reqexts v3_req -subj "/CN=localhost"
 
 # sign server CSR with CA key
 openssl x509 -req -days 10 -in server.csr -CAkey CA.key -CA CA.pem -CAcreateserial \
